6a4f8c52b72d02001b7d41ab4a8d6859aa9ec503
AA1452 <35006066+AA1452@users.noreply.github.com>
2020-06-12 13:42:01 -0500

    deleted lazyenv

--- code-base/src/main.py
+++ code-base/src/main.py
@@ -7,24 +7,24 @@ def main():
     askForProjectName()
 
 
+
 # Helper Functions
 # ------------------------------------------------------------------
+
 """First screen the user will see"""
-
-
 def titleScreen():
     print("\n\n  _                      _____ _             _            \n | |                    / ____| |           | |           \n | |     __ _ _____   _| (___ | |_ __ _ _ __| |_ ___ _ __ \n | |    / _` |_  / | | |\___ \| __/ _` | '__| __/ _ \ '__|\n | |___| (_| |/ /| |_| |____) | || (_| | |  | ||  __/ |   \n |______\__,_/___|\__, |_____/ \__\__,_|_|   \__\___|_|   \n                   __/ |                                  \n                  |___/                                   \n\n")
 
-
 def askForProjectName():
-    projectName = input("Project Name: ")
-    print("Creating Project...")
+    projectName = input("Project Name: ") 
+    print("Creating Project...") 
     if createDirectory(projectName):
-        print("Finished Creating Project")
-        print("[Summary of Project]")
-        print("Project Name: " + projectName)
-        print("Project Type: Web-app")
+        print("Finished Creating Project") 
+        print("[Summary of Project]") 
+        print("Project Name: " + projectName) 
+        print("Project Type: Web-app") 
         print("Project Location: " + os.getcwd())
+
 
 
 # Your Addition
@@ -37,9 +37,7 @@ def createDirectory(projectName):
     if not os.path.exists(projectName):
         os.makedirs(projectName)
     else:
-        print("[ERROR] A project already exists with the name  '" +
-              projectName + "' ")
-
+        print("[ERROR] A project already exists with the name  '" + projectName + "' ")
 
 # Code execution
 # ------------------------------------------------------------------
