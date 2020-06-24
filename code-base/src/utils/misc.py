50a23622d6672180c7136cd15c05bdc5c19e9920
Anurag Patil <anurag@sudodevs.com>
2020-06-25 01:20:53 +0530

    breaking: Added flag for adding gitignore(s).
    Users are now able to input the gitignores at all cases.That incl-
    udes running the file with args or with the prompt.
    
    Also a key change is that the `project_type` is the only input one
    needs to enter to get started with the project creation. If any of
    the existing flags are not provided values with, they are prompted
    to input the values.
    
    Added relevant help messages and revamped the ascii text.

---
+++ code-base/src/utils/misc.py
@@ -0,0 +1,71 @@
+import click
+import os
+from gitignore import Gitignore
+project_types = ["basic", "web", "react", "angular", "gatsby", "python"]
+
+
+def print_title():
+    click.echo(r"""
+  _      _   _____   _____ _____ _   ___ _____ ___ ___ 
+ | |    /_\ |_  \ \ / / __|_   _/_\ | _ |_   _| __| _ \
+ | |__ / _ \ / / \ V /\__ \ | |/ _ \|   / | | | _||   /
+ |____/_/ \_/___| |_| |___/ |_/_/ \_|_|_\ |_| |___|_|_\
+
+
+""")
+
+
+def check_project_type(project_type: str) -> bool:
+    if project_type.lower() in project_types:
+        return True
+    return False
+
+
+def create_directory(project_name: str):
+    """Creates the root directory to store the web app"""
+    if not os.path.exists(project_name):
+        os.makedirs(project_name)
+    else:
+        click.echo(
+            "[ERROR] A project already exists with the name  '" + project_name + "' ")
+
+
+def basic_project(project_name: str, gitignores: list) -> bool:
+    g = Gitignore()
+    if not os.path.exists(project_name):
+        os.makedirs(project_name)
+        os.chdir(project_name)
+        for gitignore in gitignores:
+            g.gitignore_to_file(gitignore)
+        os.makedirs('src')
+        return True
+    else:
+        click.echo("[ERROR] A project already exists with the name  '" +
+                   project_name + "' ")
+        return False
+
+
+def generate_project(name: str = '', gitignore_string: str = ''):
+    if name == '':
+        project_name = input("Project Name: ")
+
+    elif name:
+        project_name = name
+        gitignores = gitignore_string.split(' ')
+
+    if gitignore_string == '':
+        if (input('Do you wish to add a .gitignore file (y/n)?: ').lower() == 'y'):
+            gitignores = input(
+                'Enter gitignore(s) to add to your project: ').split(' ')
+        else:
+            gitignores = []
+    else:
+        gitignores = gitignore_string.split(' ')
+
+    click.echo("Creating Project...")
+    if basic_project(project_name, gitignores):
+        click.echo("Finished Creating Project")
+        click.echo("[Summary of Project]")
+        click.echo("Project Name: " + project_name)
+        click.echo("Project Type: Web-app")
+        click.echo("Project Location: " + os.getcwd())
