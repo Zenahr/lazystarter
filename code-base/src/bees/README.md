2b8591def18d6081edbdd889296a7847eee85828
jls47 <lukeschaefer@gmail.com>
2020-07-07 19:04:06 -0700

    barebones readme generation

--- code-base/src/bees/README.md
+++ code-base/src/bees/README.md
@@ -5,4 +5,10 @@ README
 # Overview
 
 
-This is a project made using [lazystarter](https://github.com/Zenahr/lazystarter), the static site generator for people who can't be bothered.
+Welcome to bees, a basic project made using [lazystarter](https://github.com/Zenahr/lazystarter), the static site generator for people who can't be bothered.
+
+
+# Dependencies
+
+
+This being a basic project, it should not require anything other than the base html, css, and javascript.
--- code-base/src/utils/readme.py
+++ code-base/src/utils/readme.py
@@ -13,4 +13,8 @@ class Readme:
         newRM = MdUtils(file_name='README', title='README')
         newRM.new_header(level=1, title='Overview')
         newRM.new_paragraph("Welcome to " + self.name + ", a " + self.type + " project made using [lazystarter](https://github.com/Zenahr/lazystarter), the static site generator for people who can't be bothered.")
+        newRM.new_paragraph()
+        newRM.new_header(level=1, title='Dependencies')
+        if(self.type == "basic"):
+            newRM.new_paragraph("This being a basic project, it should not require anything other than the base html, css, and javascript.")
         newRM.create_md_file()
