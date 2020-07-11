bdb8bbd73dfae99445471f080e8db7b725213c75
jls47 <lukeschaefer@gmail.com>
2020-07-10 17:10:09 -0700

    I now remember how to write to a file

--- code-base/src/utils/readme.py
+++ code-base/src/utils/readme.py
@@ -1,7 +1,5 @@
 import os
 import click
-from mdutils.mdutils import MdUtils
-from mdutils import Html
 
 class Readme:
     def __init__(self, name, type, tech):
@@ -10,11 +8,5 @@ class Readme:
         self.tech = tech
         
     def write(self):
-        newRM = MdUtils(file_name='README', title='README')
-        newRM.new_header(level=1, title='Overview')
-        newRM.new_paragraph("Welcome to " + self.name + ", a " + self.type + " project made using [lazystarter](https://github.com/Zenahr/lazystarter), the static site generator for people who can't be bothered.")
-        newRM.new_paragraph()
-        newRM.new_header(level=1, title='Dependencies')
-        if(self.type == "basic"):
-            newRM.new_paragraph("This being a basic project, it should not require anything other than the base html, css, and javascript.")
-        newRM.create_md_file()
+        with open('README.md', 'w') as readme:
+            readme.write('aaaaaaaa')
